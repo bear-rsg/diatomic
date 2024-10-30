773b8f03e1a9564dce770d9dc1da14ea6829ab71
Cerys <39588572+ceryslewis@users.noreply.github.com>
2024-10-30 16:36:38 +0000

    Update orig.js

--- js/orig.js
+++ js/orig.js
@@ -735,47 +735,6 @@
         map.on('idle', stopSpinner);
     });
 
-    map.on('style.load', () => {        
-        const layers = map.getStyle().layers;
-        const labelLayerId = layers.find(
-            (layer) => layer.type === 'symbol' && layer.layout['text-field']
-        ).id;
-
-        map.addLayer(
-            {
-                'id': 'add-3d-buildings',
-                'source': 'composite',
-                'source-layer': 'building',
-                'filter': ['==', 'extrude', 'true'],
-                'type': 'fill-extrusion',
-                'minzoom': 15,
-                'paint': {
-                    'fill-extrusion-color': '#e8f3f7',
-                    
-                    'fill-extrusion-height': [
-                        'interpolate',
-                        ['linear'],
-                        ['zoom'],
-                        15,
-                        0,
-                        15.05,
-                        ['get', 'height']
-                    ],
-                    'fill-extrusion-base': [
-                        'interpolate',
-                        ['linear'],
-                        ['zoom'],
-                        15,
-                        0,
-                        15.05,
-                        ['get', 'min_height']
-                    ],
-                    'fill-extrusion-opacity': 0.85
-                }
-            },
-            labelLayerId
-        );
-    });
 
     window.onload = function() {
         startSpinner();
